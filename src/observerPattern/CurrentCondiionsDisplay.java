e4154ce3ee0350be3a4f2851721ed93968d2b059
ABlued <skwodhkswkd@naver.com>
2022-07-17 14:10:29 +0900

    feat : Observer 구현

---
+++ src/observerPattern/CurrentCondiionsDisplay.java
@@ -0,0 +1,24 @@
+package observerPattern;
+
+public class CurrentCondiionsDisplay implements Observer, DisplayElement{
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentCondiionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("현재 상태: 온도 " + temperature + "F, temperature " + humidity + "%");
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+}
