a551f1401c9c3c289bbc2608fe5afece12c4c5f7
ABlued <skwodhkswkd@naver.com>
2023-04-16 16:39:57 +0900

    feat : 음료 decorator 구현

---
+++ src/observerPattern/Main.java
@@ -0,0 +1,15 @@
+package observerPattern;
+
+public class Main {
+    public static void main(String[] args){
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+        weatherData.setMesurements(80, 65, 30.4f);
+        weatherData.setMesurements(82, 70, 29.2f);
+        weatherData.setMesurements(78, 90, 29.2f);
+    }
+}
